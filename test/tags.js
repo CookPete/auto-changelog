1969177241b0829ee9716af9280aaed667434e68
Chuck Mastrandrea <Chuck.Mastrandrea@sas.com>
2021-07-13 10:20:42 -0400

    Add ending-version option

--- test/tags.js
+++ test/tags.js
@@ -113,6 +113,10 @@ describe('fetchTags', () => {
     expect(await fetchTags({ ...options, startingDate: '2000-05-01' })).to.have.lengthOf(1)
   })
 
+  it('supports --ending-version', async () => {
+    expect(await fetchTags({ ...options, endingVersion: 'v0.2.2' })).to.have.lengthOf(3)
+  })
+
   it('supports partial semver tags', async () => {
     mock('cmd', () => Promise.resolve([
       'v0.1---2000-02-01',
